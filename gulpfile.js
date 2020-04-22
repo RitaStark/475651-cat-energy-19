28a6db52450876f18fd695e5da7eb2c8d1e6cf89
RitaStark <ritastar13@gmail.com>
2020-04-22 21:18:35 +0300

    add webp/svgstore to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
 
 
 gulp.task("css", function () {
@@ -41,7 +43,7 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-console.error("hhhh");
+
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png, jpg, svg}")
     .pipe(imagemin([
@@ -52,6 +54,22 @@ gulp.task("images", function () {
 
     .pipe(gulp.dest("source/img"));
 
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png, jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img"));
+})
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("start", gulp.series("css", "server"));
