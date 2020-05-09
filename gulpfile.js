93408ad0e6a582fa3ea6f71a17e0c44fd7f9afb1
RitaStark <ritastar13@gmail.com>
2020-05-09 14:15:47 +0300

    htmlmin - not added

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+// var htmlmin = require("gulp-htmlmin");
 
 
 gulp.task("css", function () {
@@ -86,6 +87,8 @@ gulp.task("html", function () {
     .pipe(posthtml([
       include()
     ]))
+    // .pipe(htmlmin())
+    // .pipe(rename("*.min.html"))
     .pipe(gulp.dest("build"));
 });
 
